bac09498311afc4f93dab84ee71f52e6f3c4138d
Max Emerson <maxemerson2013@gmail.com>
2024-02-28 09:29:24 +0000

    finished workshop on sql

--- nextjsworkshop/src/app/posts/[id]/page.jsx
+++ nextjsworkshop/src/app/posts/[id]/page.jsx
@@ -1,3 +1,5 @@
+import { sql } from "@vercel/postgres";
+
 export async function generateMetadata({ params }) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${params.id}`
@@ -10,15 +12,30 @@ export async function generateMetadata({ params }) {
 }
 
 export default async function Page({ params }) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${params.id}`
-  );
-  const post = await response.json();
+  const post = (
+    await sql`
+  SELECT * FROM posts WHERE id = ${params.id};`
+  ).rows[0];
+  console.log(post);
   return (
     <div>
       <h1> Post {post.id}</h1>
       <h2>{post.title}</h2>
-      <p>{post.body}</p>
+      <p>{post.content}</p>
     </div>
   );
 }
+
+// export default async function Page({ params }) {
+//   const response = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${params.id}`
+//   );
+//   const post = await response.json();
+//   return (
+//     <div>
+//       <h1> Post {post.id}</h1>
+//       <h2>{post.title}</h2>
+//       <p>{post.body}</p>
+//     </div>
+//   );
+// }
--- nextjsworkshop/src/app/posts/page.jsx
+++ nextjsworkshop/src/app/posts/page.jsx
@@ -1,22 +1,39 @@
-import Link from "next/link";
-export default async function PostsPage({ searchParams }) {
-  console.log("searchParams", searchParams);
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const posts = await response.json();
-  if (searchParams.sort === "desc") {
-    posts.reverse();
-  }
+import { sql } from "@vercel/postgres";
+// import Link from "next/link";
+
+export default async function PostsPage() {
+  const posts = await sql`
+  SELECT * FROM posts;`;
 
   return (
     <div>
       <h1>Posts</h1>
-      <Link href="/posts?sort=asc">Sort ascending</Link>
-      <Link href="/posts?sort=desc">Sort descending</Link>
       <ul>
-        {posts.map((post) => (
+        {posts.rows.map((post) => (
           <li key={post.id}>{post.title}</li>
         ))}
       </ul>
     </div>
   );
 }
+// export default async function PostsPage({ searchParams }) {
+//   console.log("searchParams", searchParams);
+//   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+//   const posts = await response.json();
+//   if (searchParams.sort === "desc") {
+//     posts.reverse();
+//   }
+
+//   return (
+//     <div>
+//       <h1>Posts</h1>
+//       <Link href="/posts?sort=asc">Sort ascending</Link>
+//       <Link href="/posts?sort=desc">Sort descending</Link>
+//       <ul>
+//         {posts.map((post) => (
+//           <li key={post.id}>{post.title}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
